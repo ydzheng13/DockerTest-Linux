aa80427158b91920a30d30a395acc4744c6ac112
ydzheng13 <52845683+ydzheng13@users.noreply.github.com>
2021-04-08 11:37:24 -0500

    Add project files.

---
+++ DockerTest-Linux/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DockerTest_Linux.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
